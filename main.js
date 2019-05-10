02a107c7c40887ff01b100f48f39ecc44cf87a0d
jculver1 <jculver1900@gmail.com>
2019-05-09 19:07:40 -0600

    tip calculator working, needs styling

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const calculateTip = () => {
     let billAmount = document.querySelector('#amount').value
     let percentage = document.querySelector('#percent').value
     let tip = (billAmount * percentage) / 100
+    document.querySelector('h2').innerHTML = tip 
+   
 }
 
 
